82880ca4bf5820db5b0d6570f249bc707d81a4f6
Ritesh <riteshgharti121232@gmail.com>
2022-11-25 12:38:52 +0530

    Updates

---
+++ precache-manifest.6ec21b79ac5e1dde2ccd16eb9c1e71eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82ef7d3ffd06dc90f196",
+    "url": "/aionewsapi/static/css/main.f786e973.chunk.css"
+  },
+  {
+    "revision": "82ef7d3ffd06dc90f196",
+    "url": "/aionewsapi/static/js/main.daac611f.chunk.js"
+  },
+  {
+    "revision": "9278d7e7f08db3974f18",
+    "url": "/aionewsapi/static/js/runtime~main.29e2705f.js"
+  },
+  {
+    "revision": "f7abdefb47f0728154d4",
+    "url": "/aionewsapi/static/js/2.935230a3.chunk.js"
+  },
+  {
+    "revision": "74669d0c7b4b1966eb1d",
+    "url": "/aionewsapi/static/js/3.6a7f9668.chunk.js"
+  },
+  {
+    "revision": "5e3d77f858a38660487c26b9e664471c",
+    "url": "/aionewsapi/index.html"
+  }
+];
